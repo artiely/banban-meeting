5ec743c70968346590e891aa46bcdad6e2308059
artiely <1119696785@qq.com>
2020-12-16 18:45:20 +0800

    a

---
+++ src/agora-rtm-client.js
@@ -0,0 +1,139 @@
+import AgoraRTM from "agora-rtm-sdk";
+import EventEmitter from "events";
+import {v4 as uuid} from "uuid";
+import Danmaku from "danmaku/dist/esm/danmaku.dom.js";
+let _uid = `${uuid()}`;
+export default class RTMClient {
+  constructor() {
+    // Options for joining a channel
+    this.option = {
+      appId: "21d0c10aec25420bab36a6b67bc3ad4d",
+      channel: "banban",
+      uid: _uid,
+      token: "",
+    };
+    this.client = null;
+    this.channel = null;
+    this._eventBus = new EventEmitter();
+    this.danmaku = null
+  }
+  initDanmuku(){
+    this.danmaku = new Danmaku({
+      container: document.getElementById("vs"),
+      comments: [],
+      engine: "DOM",
+      speed: 144,
+    });
+    window.addEventListener("resize", this.resize);
+  }
+  resize() {
+    this.danmaku.resize();
+  }
+  sendComment(message) {
+    console.log("🚀 ~ file: agora-rtm-client.js ~ line 34 ~ RTMClient ~ sendComment ~ this.danmaku", this.danmaku)
+    this.danmaku.emit({
+      text: message.text,
+      // 默认为 rtl（从右到左），支持 ltr、rtl、top、bottom。
+      mode: "bottom",
+      // 在使用 DOM 引擎时，该函数应当返回一个 HTMLElement。
+      render: function () {
+        var $div = document.createElement("div");
+        let style = {
+          fontSize: "20px",
+          color: "#333",
+          border: "1px solid #337ab7",
+          ...message.style,
+        };
+        $div.style.fontSize = style.fontSize;
+        $div.style.color = style.color;
+        $div.style.baorder = style.baorder;
+        $div.style.textShadow = style.textShadow;
+        $div.style.background = style.background;
+
+        $div.innerHTML = message.name + ":" + message.text;
+        return $div;
+      },
+    });
+  }
+  // 登录
+  login() {
+    this.client = AgoraRTM.createInstance(this.option.appId);
+    const client = this.client;
+    this.initDanmuku()
+
+    return new Promise((resolve, reject) => {
+      client
+        .login({ token: null, uid: _uid })
+        .then(() => {
+          console.log("AgoraRTM client login success");
+          resolve();
+        })
+        .catch((err) => {
+          console.log("AgoraRTM client login failure", err);
+          reject();
+        });
+    });
+  }
+  // 加入频道
+  joinChannel() {
+    return new Promise((resolve, reject) => {
+      this.login().then(() => {
+        const client = this.client;
+        // 创建并加入频道
+        this.channel = client.createChannel(this.option.channel); // 此处传入频道 ID
+        this.channel
+          .join()
+          .then(() => {
+            this.clientListener();
+            /* 加入频道成功的处理逻辑 */
+            resolve();
+          })
+          .catch((error) => {
+            /* 加入频道失败的处理逻辑 */
+            reject(error);
+          });
+      });
+    });
+  }
+  sendMessage(text) {
+    return new Promise((resolve, reject) => {
+      this.channel
+        .sendMessage({ text })
+        .then((res) => {
+          /* 频道消息发送成功的处理逻辑 */
+          resolve(res);
+        })
+        .catch((error) => {
+          /* 频道消息发送失败的处理逻辑 */
+          reject(error);
+        });
+    });
+  }
+  clientListener() {
+    const client = this.client;
+
+    // 监听连接状态改变
+    client.on("ConnectionStateChanged", (newState, reason) => {
+      console.log(
+        "on connection state changed to " + newState + " reason: " + reason
+      );
+      this._eventBus.emit("ConnectionStateChanged", newState, reason);
+    });
+
+    this.channel.on("ChannelMessage", ({ text }, senderId) => {
+      // text 为收到的频道消息文本，senderId 为发送方的 User ID
+      /* 收到频道消息的处理逻辑 */
+      this._eventBus.emit("ChannelMessage", { text }, senderId);
+
+      this.sendComment(JSON.parse(text))
+    });
+  }
+
+  on(eventName, callback) {
+    this._eventBus.on(eventName, callback);
+  }
+
+  leaveChannel() {
+   
+  }
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,214 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-if="!joined">
+      <md-input-item
+        preview-type="text"
+        v-model="name"
+        title="真实姓名或花名"
+        maxlength="8"
+        placeholder="请输入真实姓名或花名"
+        is-title-latent
+        :error="error_name"
+      ></md-input-item>
+      <br />
+      <md-button type="primary" @click="greet">加入</md-button>
+    </div>
+    <div v-else>
+      <md-input-item
+        preview-type="text"
+        v-model="message"
+        title="请输入"
+        placeholder="请输入"
+        :error="error_message"
+        is-title-latent
+      ></md-input-item>
+      <md-button type="primary" @click="send">发送</md-button>
+
+      <md-button @click="danmuSettingVisible = true">设置弹幕样式</md-button>
+      <md-icon name="message" size="lg"></md-icon>
+      <div
+        class="messageitem"
+        :class="item.uid ? '' : 'active'"
+        v-for="(item, i) in msgList"
+        :key="i"
+      >
+        {{ item.name }}:{{ item.text }}
+      </div>
+      <span ref="link"></span>
+    </div>
+    <div
+      ref="danmuArea"
+      id="vs"
+      style="
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+        background: #fafafa;
+      "
+    ></div>
+    <!-- 弹幕设置 -->
+    <md-popup v-model="danmuSettingVisible" position="right">
+      <div
+        class="md-example-popup md-example-popup-right"
+        style="height: 100%; width: 300px; background: #fff"
+      >
+        样式
+
+        <md-field>
+          <md-cell-item title="弹幕颜色">
+            <input
+              type="color"
+              class="color"
+              v-model="style.color"
+              slot="right"
+            />
+          </md-cell-item>
+          <md-cell-item title="弹幕背景">
+            <input
+              type="color"
+              class="color"
+              v-model="style.background"
+              slot="right"
+            />
+          </md-cell-item>
+          <md-cell-item title="弹幕大小" brief="滑动下方卡尺设置">
+            <span slot="right">{{ fontSize }}</span>
+          </md-cell-item>
+          <md-ruler
+            :scope="[10, 40]"
+            :step="10"
+            :unit="1"
+            :max="40"
+            :min="10"
+            v-model="fontSize"
+          ></md-ruler>
+        </md-field>
+      </div>
+    </md-popup>
   </div>
 </template>
 
 <script>
+import {
+  Button,
+  InputItem,
+  Icon,
+  Popup,
+  Field,
+  CellItem,
+  RadioBox,
+  Ruler,
+} from "mand-mobile";
+import RTMClient from "../agora-rtm-client";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    "md-button": Button,
+    "md-input-item": InputItem,
+    "md-icon": Icon,
+    "md-popup": Popup,
+    [Field.name]: Field,
+    [CellItem.name]: CellItem,
+    [RadioBox.name]: RadioBox,
+    [Ruler.name]: Ruler,
+  },
+  data() {
+    return {
+      danmuSettingVisible: false,
+      name: "",
+      styleIndex: 0,
+      joined: false,
+      message: "",
+      error_name: "",
+      error_message: "",
+      rtm: null,
+      msgList: [],
+      danmaku: null,
+      fontSize: 20,
+      style: {
+        color: "#000",
+        background: "transparent",
+        fontSize: "20px",
+        border: "1px solid #337ab7",
+      },
+    };
+  },
+  methods: {
+    greet() {
+      if (this.name == "") {
+        this.error_name = "请认真填写哟";
+        return;
+      }
+      this.error_name = "";
+      this.joined = true;
+      this.rtm = new RTMClient();
+
+      this.rtm.joinChannel();
+
+      this.rtm.on("ChannelMessage", ({ text }, uid) => {
+        let msgobj = JSON.parse(text);
+
+        this.msgList.push({
+          uid: uid,
+          name: msgobj.name,
+          text: msgobj.text,
+        });
+      });
+    },
+    send() {
+      if (!this.message) {
+        this.error_message = "请输入内容哟";
+        return;
+      }
+      this.error_message = "";
+      let message = {
+        text: this.message,
+        style: { ...this.style, fontSize: this.fontSize + "px" },
+        name: this.name,
+      };
+      this.rtm.sendMessage(JSON.stringify(message)).then(() => {
+        this.msgList.push(message);
+        // this.message = "";
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
+.messageitem {
+  font-size: 24px;
+  color: #666;
+  padding: 10px;
+  &.active {
+    border: 1px solid #abcdef;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.color {
+  flex: 1;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.hello h1 {
+  color: #333;
+  line-height: 1.15;
+  font-size: 64px;
+  margin-bottom: 32px;
 }
-a {
-  color: #42b983;
+
+.hello h1 span {
+  position: relative;
+}
+
+.hello h1 span::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  left: 0;
+  bottom: 8px;
+  width: 100%;
+  height: 20px;
+  background: #ecf6ff;
 }
 </style>
